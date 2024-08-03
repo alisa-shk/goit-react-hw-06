aa478efdf2b65890f9c24ab913f0dff8edee79ff
alisa-shk <demaliska@gmail.com>
2024-08-03 19:07:54 +0300

    with persist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import ContactList from "../components/ContactList/ContactList";
 import ContactForm from "../components/ContactForm/ContactForm";
 import SearchBox from "../components/SearchBox/SearchBox";
+import "./App.css";
 
 
 const App = () => {
 
     return (
-        <div>
+        <div className="box">
             <h1>Phonebook</h1>
             <ContactForm />
             <SearchBox />
